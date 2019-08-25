c767189cce857325f7e3b4028093ff001a1a5be2
Jos van Olst <vanolstj@gmail.com>
2019-08-26 00:55:04 +1200

    Added todos

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
+/*Declaration of scales*/
+const ionian = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4']
+const dorian = ['C4', 'D4', 'Eb4', 'F4', 'G4', 'A4', 'Bb4']
+const phrygian = ['C4', 'Db4', 'Eb4', 'F4', 'G4', 'Ab4', 'Bb4']
+const lydian = ['C4', 'D4', 'E4', 'F#4', 'G4', 'A4', 'B4']
+const mixolydian = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'Bb4']
+const aeolian = ['C4', 'D4', 'Eb4', 'F4', 'G4', 'Ab4', 'Bb4']
+const locrian = ['C4', 'Db4', 'Eb4', 'F4', 'Gb4', 'Ab4', 'Bb4']
+
+/* TODO:
+Create a function that returns the notes from an entered chord
+such as getNotes(CMaj) = [C, E, G] etc
+Maj: Maj 3rd + Min 3rd
+Min: Min 3rd + Maj 3rd
+Dim: Min 3rd + Min 3rd
+ */
+ 
+ /*Chord progression will be I vi IV V in each mode
+Ionian: I vi IV V
+Dorian: i vi(dim) iv V
+Phrygian: i VI iv v(dim)
+Lydian: I vi iv(dim) V
+Mixolydian: I vi IV v
+Aeolian: i VI iv v
+Locrian: i(dim) VI iv V
+ */
+
+
+
 const synth = new Tone.Synth({
   oscillator: {
     type: 'triangle8'
@@ -12,18 +41,6 @@ const synth = new Tone.Synth({
 
 const polySynth = new Tone.PolySynth(6, Tone.Synth).toMaster()
 //polySynth.set('detune', -50)
-
-
-function ionian() {
-  synth.triggerAttackRelease('C3', 0.2, 0.2)
-  synth.triggerAttackRelease('D3', 0.2, 0.4)
-  synth.triggerAttackRelease('E3', 0.2, 0.6)
-  synth.triggerAttackRelease('F3', 0.2, 0.8)
-  synth.triggerAttackRelease('G3', 0.2, 1.0)
-  synth.triggerAttackRelease('A3', 0.2, 1.2)
-  synth.triggerAttackRelease('B3', 0.2, 1.4)
-  synth.triggerAttackRelease('C4', 0.2, 1.6)
-}
 
 function chordProgression() {
   // I - vi - IV - V
@@ -46,7 +63,7 @@ var ionianChords = new Tone.Part(function(time, event){
 
 var arpeggio = new Tone.Pattern(function(time, note){
   synth.triggerAttackRelease(note, '8n');
-}, ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'], 'random')
+}, dorian, 'upDown')
 
 $(document).ready(function() {
   //ionian()
@@ -55,7 +72,9 @@ $(document).ready(function() {
   // ionianChords.loopEnd = '4m'
   arpeggio.start(0)
   Tone.Transport.start(0)
-
+  document.getElementById('test').addEventListener('click', function() {
+    arpeggio.stop()
+  })
 
   //chordProgression()
 })
