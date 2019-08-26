b107bd984a95328e6842992d5c0724c82f6a66d8
Jos van Olst <vanolstj@gmail.com>
2019-08-26 14:07:33 +1200

    getChord implemented

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const mixolydian = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'Bb4']
 const aeolian = ['C4', 'D4', 'Eb4', 'F4', 'G4', 'Ab4', 'Bb4']
 const locrian = ['C4', 'Db4', 'Eb4', 'F4', 'Gb4', 'Ab4', 'Bb4']
 
+
 /* TODO:
 Create a function that returns the notes from an entered chord
 such as getNotes(CMaj) = [C, E, G] etc
@@ -14,7 +15,25 @@ Maj: Maj 3rd + Min 3rd
 Min: Min 3rd + Maj 3rd
 Dim: Min 3rd + Min 3rd
  */
- 
+function getChord(tonic = 'C', type = 'maj'){
+  let tonicIndexes = {'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11}
+  let notes = ['C4', 'C#4', "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"]
+
+  let tonicIndex = tonicIndexes[tonic]
+
+  if (type == 'min'){
+    return [notes[tonicIndex], notes[(tonicIndex + 3) % 11], notes[(tonicIndex + 7) % 11]]
+  }
+  else if (type == 'maj'){
+    return [notes[tonicIndex], notes[(tonicIndex + 4) % 11], notes[(tonicIndex + 7) % 11]]
+  }
+  else if (type == 'dim'){
+    return [notes[tonicIndex], notes[(tonicIndex + 3) % 11], notes[(tonicIndex + 6) % 11]]
+  }
+}
+
+console.log(getChord())
+
  /*Chord progression will be I vi IV V in each mode
 Ionian: I vi IV V
 Dorian: i vi(dim) iv V
@@ -29,11 +48,11 @@ Locrian: i(dim) VI iv V
 
 const synth = new Tone.Synth({
   oscillator: {
-    type: 'triangle8'
+    type: 'sine'
   },
   envelope: {
-    attack: 1.5,
-    decay: 1,
+    attack: 1.5, //0 - 2
+    decay: 1, //0 - 2
     sustain: 0.4,
     release: 4
   }
@@ -54,10 +73,10 @@ var ionianChords = new Tone.Part(function(time, event){
   polySynth.triggerAttackRelease(event.note, event.dur, time)
 },
 [
-  {time: '0:0:0', note: ['C4', 'E4', 'G4'], dur: '1m'},
-  {time: '1:0:0', note: ['A4', 'C4', 'E4'], dur: '1m'},
-  {time: '2:0:0', note: ['F4', 'A4', 'C4'], dur: '1m'},
-  {time: '3:0:0', note: ['G4', 'B4', 'D4'], dur: '1m'},
+  {time: '0:0:0', note: getChord('C', 'maj'), dur: '1m'},
+  {time: '1:0:0', note: getChord('A', 'min'), dur: '1m'},
+  {time: '2:0:0', note: getChord('F', 'maj'), dur: '1m'},
+  {time: '3:0:0', note: getChord('G', 'maj'), dur: '1m'},
 
 ])
 
@@ -72,7 +91,8 @@ $(document).ready(function() {
   // ionianChords.loopEnd = '4m'
   arpeggio.start(0)
   Tone.Transport.start(0)
-  document.getElementById('test').addEventListener('click', function() {
+  document.getElementById('mute').addEventListener('click', function() {
+    console.log('test')
     arpeggio.stop()
   })
 
